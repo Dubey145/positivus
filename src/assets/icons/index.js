d38aa810e0fb49ff8c6304300856bf2940423cf6
Aayush Dubey <91815836+Dubey145@users.noreply.github.com>
2024-08-03 22:22:08 +0530

    add: mob support for hero section

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,54 +1,67 @@
-import starBlack from './star-black.svg'
-import hamburger from './hamburger.svg'
-import greenArrow from './green-arrow.svg'
-import blackArrow from './black-arrow.svg'
-import plus from './plus.svg'
-import minus from './minus.svg'
-import linkedin from './linkedin.svg'
-import whiteStar from './white-star.svg'
-import socials from './socials.svg'
-import starPlus from './star-plus.svg'
-import starCross from './star-cross.svg'
-import heart from './heart.svg'
-import location from './location.svg'
-import play from './play.svg'
-import share from './share.svg'
-import megaphone from './megaphone.svg'
-import ring from './ring.svg'
-import straightRing from './straight-ring.svg'
-import spikeStar from './spike-star.svg'
-import greenStarPlus from './green-star-plus.svg'
-import grayStar from './gray-star.svg'
-import spikeCircle from './spike-circle.svg'
-import largeGreenStar from './large-green-star.svg'
+import starBlack from "./star-black.svg";
+import hamburger from "./hamburger.svg";
+import greenArrow from "./green-arrow.svg";
+import blackArrow from "./black-arrow.svg";
+import plus from "./plus.svg";
+import minus from "./minus.svg";
+import linkedin from "./linkedin.svg";
+import whiteStar from "./white-star.svg";
+import socials from "./socials.svg";
+import starPlus from "./star-plus.svg";
+import starCross from "./star-cross.svg";
+import heart from "./heart.svg";
+import location from "./location.svg";
+import play from "./play.svg";
+import share from "./share.svg";
+import megaphone from "./megaphone.svg";
+import ring from "./ring.svg";
+import straightRing from "./straight-ring.svg";
+import spikeStar from "./spike-star.svg";
+import greenStarPlus from "./green-star-plus.svg";
+import grayStar from "./gray-star.svg";
+import spikeCircle from "./spike-circle.svg";
+import mobHeart from "./mob-heart.svg";
+import mobHeroBackground from "./mob-hero-background.svg";
+import mobLocation from "./mob-location.svg";
+import mobMegaphone from "./mob-megaphone.svg";
+import mobPlay from "./mob-play.svg";
+import mobShare from "./mob-share.svg";
+import mobStarPlus from "./mob-star-plus.svg";
+import mobStarCross from "./mob-star-cross.svg";
 import largeBlackStar from './large-black-star.svg'
-
-
-
+import largeGreenStar from './large-green-star.svg'
 
 export {
-    starBlack,
-    hamburger,
-    greenArrow,
-    blackArrow,
-    plus,
-    minus,
-    linkedin,
-    whiteStar,
-    socials,
-    starPlus,
-    starCross,
-    heart,
-    location,
-    play,
-    share,
-    megaphone,
-    ring,
-    straightRing,
-    spikeStar,
-    greenStarPlus,
-    grayStar,
-    spikeCircle,
-    largeGreenStar,
-    largeBlackStar
-}
+  starBlack,
+  hamburger,
+  greenArrow,
+  blackArrow,
+  plus,
+  minus,
+  linkedin,
+  whiteStar,
+  socials,
+  starPlus,
+  starCross,
+  heart,
+  location,
+  play,
+  share,
+  megaphone,
+  ring,
+  straightRing,
+  spikeStar,
+  greenStarPlus,
+  grayStar,
+  spikeCircle,
+  largeGreenStar,
+  largeBlackStar,
+  mobHeart,
+  mobHeroBackground,
+  mobLocation,
+  mobMegaphone,
+  mobPlay,
+  mobShare,
+  mobStarPlus,
+  mobStarCross,
+};
